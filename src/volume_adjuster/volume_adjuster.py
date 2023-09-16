d44171220d9cc12956be25c7896cfed313238aaa
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-09-16 22:18:05 +0100

    Update docstrings

--- src/volume_adjuster/volume_adjuster.py
+++ src/volume_adjuster/volume_adjuster.py
@@ -11,10 +11,16 @@ class VolumeAdjuster:
     """Volume adjuster class"""
 
     def __init__(self, dir_path: str, extensions: list[str], target_dbfs: float):
-        # Create type checker
+        """Initialise the member variables.
+
+        :param str dir_path: a path to a target directory
+        :param list[str] extensions: a list of a target extensions
+        :param float target_dbfs: a target decibel relative to the maximum possible loudness
+        """
+        # Create TypeChecker object.
         self.__type_checker = TypeChecker()
 
-        # Check types
+        # Check the types of the arguments.
         self.__type_checker.validate_basic_type("dir_path", dir_path, str)
         self.__type_checker.validate_basic_type("extensions", extensions, list)
         self.__type_checker.validate_basic_type("dir_path", dir_path, str)
@@ -23,6 +29,7 @@ class VolumeAdjuster:
         # Initialise member variables.
         self.__dir_path: str = dir_path
         self.__extensions: list[str] = map(
+            # Add if there is no "." at the top of the extension
             lambda x: x if x[0] == "." else "." + x, extensions
         )
         self.__target_dbfs: float = target_dbfs
@@ -33,7 +40,7 @@ class VolumeAdjuster:
 
     def __set_all_targets(self):
         """Set all target file paths to self.__target_file_paths."""
-        # Get all file paths.
+        # Get all file paths under the target directory.
         all_file_paths: list[str] = glob.glob(os.path.join(self.__dir_path, "*"))
 
         # Extract only file paths whose extension is in self.__extensions.
@@ -46,10 +53,18 @@ class VolumeAdjuster:
 
     @staticmethod
     def adjust(audio_data: AudioSegment, target_dbfs: float) -> AudioSegment:
+        """Adjust the volume of the audio data by the given target decibel relative to the maximum possible loudness.
+
+        :param AudioSegment audio_data: a target audio data
+        :param float target_dbfs: a target decibel relative to the maximum possible loudness
+        :return AudioSegment: the adjusted audio data
+        """
         diff_dbfs = audio_data - target_dbfs
         return audio_data - diff_dbfs
 
     def __adjust_all_targets(self):
+        """Adjust the volumes of all target audio data.
+        """
         self.__adjusted_data = list(
             map(
                 functools.partial(self.adjust, target_dbfs=self.__target_dbfs),
@@ -57,7 +72,9 @@ class VolumeAdjuster:
             )
         )
 
-    def __export_adjusted_data(self):
+    def __save_adjusted_data(self):
+        """Save all adjusted target audio data.
+        """
         for target_file_path, adjusted_data in zip(
             self.__target_file_paths, self.__adjusted_data
         ):
@@ -66,6 +83,8 @@ class VolumeAdjuster:
             )
 
     def run(self):
+        """Load all target audio data, adjust the volumes and save them.
+        """
         self.__set_all_targets()
         self.__adjust_all_targets()
-        self.__export_adjusted_data()
+        self.__save_adjusted_data()
