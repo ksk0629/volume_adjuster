ed23ac7a1aaa9da2415852fb05a05d466a8505f4
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-09-17 00:53:40 +0100

    Add main

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# current_dir=$(dirname "$0")
+# cd $current_dir
+
+python ./src/volume_adjuster/main.py -c ./config.yaml
---
+++ src/volume_adjuster/main.py
@@ -0,0 +1,22 @@
+import argparse
+
+import yaml
+
+from src.volume_adjuster.volume_adjuster import VolumeAdjuster
+
+
+def main(config_yaml_path):
+    with open(config_yaml_path, "r") as yaml_f:
+        config = yaml.safe_load(yaml_f)
+
+    volume_adjuster = VolumeAdjuster(**config)
+    
+    volume_adjuster.run()
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Adjust volumes of audio files under a given directory.")
+    parser.add_argument("-c", "--config_yaml_path", required=True, type=str)
+    args = parser.parse_args()
+    
+    main(args.config_yaml_path)
