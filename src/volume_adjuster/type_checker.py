14fbcb359aebacca076d6a6723702bc9e2d97052
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-09-16 22:04:15 +0100

    Add docstrings

--- src/volume_adjuster/type_checker.py
+++ src/volume_adjuster/type_checker.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class TypeChecker:
     """Type checker class"""
 
@@ -8,22 +5,36 @@ class TypeChecker:
         pass
 
     def validate_basic_type(self, object_name: str, object: any, type: any):
+        """Validate if the type of the given object is as same as the given type.
+
+        :param str object_name: the name of the object
+        :param any object: the target object
+        :param any type: the expected type of the object
+        :raises TypeError: raise if the type of the given object is not as same as the given type.
+        """
         if not isinstance(object, type):
-            msg: str = f"""The type of {object_name} has to be {type}.
+            msg: str = f'''The type of {object_name} has to be {type}.
             But it is {type(object)}.
-            """
+            '''
             raise TypeError(msg)
 
     def validate_list_elements_type(
-        self, object_name: str, target_list: List[any], type: any
+        self, object_name: str, target_list: list[any], type: any
     ):
+        """Validate if the type of every element of the given list is as same as tye given type.
+
+        :param str object_name: the name of the object
+        :param List[any] target_list: the target list
+        :param any type: the expected type of every element
+        :raises TypeError: raise if the type of every element of the given list is no as same as the given type.
+        """
         try:
             for element in target_list:
                 self.validate_basic_type(
                     object_name=object_name, object=element, type=type
                 )
         except:
-            msg: str = f"""The type of every elements in {object_name} has to be {type}.
+            msg: str = f'''The type of every elements in {object_name} has to be {type}.
             But at least one of them is not.
-            """
+            '''
             raise TypeError(msg)
