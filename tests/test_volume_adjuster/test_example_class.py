3f1a88fb0a5be9a45fdfb51e4f11053e61322de8
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-09-16 23:24:19 +0100

    Initialise

--- tests/test_volume_adjuster/test_example_class.py
+++ tests/test_volume_adjuster/test_example_class.py
@@ -1,31 +1,10 @@
 import pytest
 
-from src.example_package.example_class import ExampleClass
+from src.volume_adjuster.volume_adjuster import VolumeAdjuster
 
 
-class TestExampleClassClass:
-    @pytest.mark.example_class
-    @pytest.mark.parametrize("whole_number", [1, -1, 2, -2, 0])
-    def test_integer(
-        self,
-        whole_number,
-        fixture_function,
-        fixture_class,
-        fixture_module,
-        fixture_session,
-    ):
-        example_class = ExampleClass(whole_number)
-        assert example_class.number == whole_number
-
-    @pytest.mark.example_class
-    @pytest.mark.parametrize("float_number", [0.1, -0.1, 0.2, -0.2, float(0.0)])
-    def test_with_floats(
-        self,
-        float_number,
-        fixture_function,
-        fixture_class,
-        fixture_module,
-        fixture_session,
-    ):
-        with pytest.raises(TypeError):
-            ExampleClass(float_number)
+class TestVolumeAdjuster:
+    @pytest.mark.volume_adjuster
+    @pytest.fixture
+    def test_normal_init(self):
+        pass
